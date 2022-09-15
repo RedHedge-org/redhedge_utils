53fdd94233633360f905d89bad157a378e08367d
Enrico Gandini <enricogandini93@gmail.com>
2022-09-15 12:05:45 +0200

    Improve speed of get_mongo_uri function

--- utils.py
+++ utils.py
@@ -9,11 +9,10 @@ _KEY_LOCAL_ENVIRONMENT = "local"
 
 def get_mongo_uri():
     """get the mongo-uri from the open-faas secrets or from .env file"""
-    if "MONGO_URI" in os.environ:
-        return os.environ["MONGO_URI"]
-    uri = ""
-    with open("/var/openfaas/secrets/mongo-uri") as f:
-        uri = f.read()
+    uri = os.environ.get("MONGO_URI", None)
+    if uri is None:
+        with open("/var/openfaas/secrets/mongo-uri") as f:
+            uri = f.read()
     return uri
 
 
