cf3fcfee0c05372f778437f3979828f477f40921
Nano112 <harrisonbastian.nano@gmail.com>
2022-09-16 12:42:49 +0100

    added bdp_wraper to the utils

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+import json
 import os
-
+from urllib import response
+import pandas as pd
+import requests 
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,3 +22,23 @@ def get_mongo_uri():
 def is_local():
     """check if the function is running locally."""
     return os.environ.get("ENVIRONMENT", None) == _KEY_LOCAL_ENVIRONMENT
+
+def get_env(env_name):
+    """get the environment variable from the open-faas secrets or from .env file"""
+    env = os.environ.get(env_name, None)
+    if env is None:
+        with open(f"/var/openfaas/secrets/{env_name}") as f:
+            env = f.read()
+    return env
+
+def bdp_wrapper(tickers = [], fields = [], YAS_YIELD_FLAG=None):
+    """wrapper for the function to check if the function is running locally or not"""
+    if is_local():
+        return pd.DataFrame.empty
+    url = get_env("bloomberg-api-url")
+    if url is None:
+        raise Exception("bloomberg-api-url is not set")
+    payload = json.dumps({"tickers": tickers, "fields": fields, "YAS_YIELD_FLAG": YAS_YIELD_FLAG})
+    response = requests.post(url, data=payload)
+    return response.json()
+    
