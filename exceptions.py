9c9cfe583ed40ed91f8bc1499139f833a3cfd25e
Enrico Gandini <enricogandini93@gmail.com>
2022-11-02 11:46:29 +0100

    add exception utils

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+"""
+Utilities to handle exceptions
+"""
+
+
+def get_exception_info(exc: Exception) -> dict:
+    """
+    Get a dictionary containing information about an exception.
+
+    This dictionary should be appended to the 'errors' field of responses.
+    """
+    return {
+        "message": str(exc),
+        "type": type(exc),
+        "lineno": exc.__traceback__.tb_lineno,
+    }
